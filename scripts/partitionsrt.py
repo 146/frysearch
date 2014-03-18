a53bcd6af4916980b629e3b19a9b6635af5c7e8f
Albert Sheu <albert.sheu@gmail.com>
2014-03-18 12:44:52 -0700

    Use option parsing for the build script.

--- scripts/partitionsrt.py
+++ scripts/partitionsrt.py
@@ -4,6 +4,8 @@ import sys
 import re
 import sqlite3
 import os
+import commands
+from optparse import OptionParser
 
 BLOCK_GAP = 1.0
 BLOCK_MARGIN = 0.5
@@ -112,41 +114,78 @@ def combinesrt(units):
 	ret = [block for block in ret if block.text()]
 	return ret
 
+def preprocess(vid_fname):
+	track_info = commands.getoutput('mkvmerge -i {vid_fname}'.format(vid_fname=vid_fname))
+	subtitle_field = None
+	for line in track_info.splitlines():
+		if line.startswith('Track ID ') and 'subtitles' in line:
+			subtitle_field = int(line.split()[2].rstrip(':'))
+			break
+	vid_prefix = vid_fname[:-len('.mkv')]
+	assert subtitle_field != None, "Could not find subtitle track."
+
+	extract_cmd = "mkvextract tracks {vid_fname} {subtitle_field}:{vid_prefix}.sub".format(
+		subtitle_field=subtitle_field,
+		vid_fname=vid_fname,
+		vid_prefix=vid_prefix)
+	assert os.system(extract_cmd) == 0, "Unable to extract subtitles field."
+
+	convert_cmd = r"""vobsub2srt --blacklist "|\\/\`_~<>" """ + vid_prefix
+	assert os.system(convert_cmd) == 0, "Unable to convert VobSub to SRT file."
+
+optparser = OptionParser()
+optparser.add_option('-d', '--db',  help="Filename of the quotes.db output.", dest="db_fname")
+optparser.add_option('-v', '--vid', help="Filename of the video file.", dest="vid_fname")
+optparser.add_option('-s', '--srt', help="Filename of the .srt file.", dest="srt_fname")
+optparser.add_option('-o', '--out', help="Directory to place output files.", dest="output_dname")
+
 if __name__ == '__main__':
-	command = sys.argv[1]
-	assert command in ('--info', '--index'), "Invalid command."
+	(options, args) = optparser.parse_args()
 
-	fname = sys.argv[2]
-	videofile = sys.argv[3]
-	if command == '--index':
-		dbfile = sys.argv[4]
+	command = args[0]
+	assert command in ('info', 'process', 'preprocess'), "Invalid command, must select 'info' or 'process' as first argument."
+	if command == "info":
+		assert options.srt_fname, "Must specify .srt filename to read."
+	elif command == 'process':
+		assert options.srt_fname, "Must specify .srt file in order to process index."
+		assert options.vid_fname, "Must specify video file in order to process index."
+		assert options.db_fname, "Must specify database output file in order to process index."
+		assert options.output_dname, "Must specify output directory in order to process index."
+	elif command == 'preprocess':
+		assert options.vid_fname, "Must specify video file in order to preprocess."
 
-	units = splitsrt(fname)
+	if command == 'preprocess':
+		preprocess(options.vid_fname)
+		sys.exit(0)
+
+	units = splitsrt(options.srt_fname)
 	blocks = combinesrt(units)
 	totalwords = 0
 
-	if command == '--index':
-		dbconn = sqlite3.connect(dbfile)
+	if command == 'process':
+		dbconn = sqlite3.connect(db_fname)
 		dbconn.text_factory = str
 		cursor = dbconn.cursor()
 
 	for block in blocks:
-		if command == '--info':
+
+		if command == 'info':
 			print block.title()
 			print block
 			print
+			totalwords += len(block.text().split())
 
-		start = timestamp_to_seconds(block.timestamp_begin) - BLOCK_MARGIN
-		diff = timestamp_to_seconds(block.timestamp_end) - timestamp_to_seconds(block.timestamp_begin) + BLOCK_MARGIN * 2
-		if command == '--index':
-			build_cmd = "ffmpeg -y -i {fname} -ss {start} -t {diff} -sn -vcodec libx264 -acodec libmp3lame {title}.mkv".format(
-				fname=videofile,
+		if command == 'process':
+			start = timestamp_to_seconds(block.timestamp_begin) - BLOCK_MARGIN
+			diff = timestamp_to_seconds(block.timestamp_end) - timestamp_to_seconds(block.timestamp_begin) + BLOCK_MARGIN * 2
+			build_cmd = "ffmpeg -y -i {fname} -ss {start} -t {diff} -sn -vcodec libx264 -acodec libmp3lame {outfile}".format(
+				fname=options.vid_fname,
 				start=seconds_to_timestamp(start),
 				diff=diff,
-				title=block.title()
+				outfile=os.path.join(options.output_dname, block.title() + '.mkv')
 				)
-			screencap_cmd = "ffmpeg -y -i {title}.mkv -ss 00:00:01 -vframes 1 {title}.mkv.jpg".format(
-				title=block.title())
+			screencap_cmd = "ffmpeg -y -i {title}.mkv -ss 00:00:01 -vframes 1 {outfile}".format(
+				outfile=os.path.join(options.output_dname, block.title() + '.mkv.jpg'))
 
 			assert os.system(build_cmd) == 0, "Build command failed: %s" % build_cmd
 			assert os.system(screencap_cmd) == 0, "Screencap command failed: %s" % screencap_cmd
@@ -156,13 +195,11 @@ if __name__ == '__main__':
 				(block.title(), block.title() + '.mkv', block.title() + '.mkv.jpg', block.text())
 				)
 
-		totalwords += len(block.text().split())
-
-	if command == '--info':
+	if command == 'info':
 		average = float(totalwords) / len(blocks)
 		print 'Number of blocks: %d' % len(blocks)
 		print 'Average block length: %d words' % average
 
-	if command == '--index':
+	if command == 'process':
 		dbconn.commit()
 		dbconn.close()
